fbf02ac7acbd2edc0cb4d82751c9e2a4c51e644b
Ryan Mendoza <71746816+ryan-mendoza@users.noreply.github.com>
2021-08-03 08:19:42 -0700

    init implement axios

---
+++ src/components/PostList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class PostList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return <div>List of posts</div>;
+  }
+}
+
+export default PostList;
